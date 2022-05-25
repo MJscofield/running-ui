9095cea65ab2e366027f0795a4ae68b3101fee0a
jsomin <1344359061@qq.com>
2022-05-25 21:45:01 +0800

    修复日历日期显示bug

--- src/views/calendar/index.vue
+++ src/views/calendar/index.vue
@@ -40,30 +40,32 @@ let events = ref<EventItem[]>([
     end: "2022-05-08 19:00:00",
     editable: true,
   },
+  {
+    title: "学习",
+    start: "2022-05-17 16:00:00",
+    end: "2022-05-18 19:00:00",
+    editable: true,
+  },
 ]);
 let dateClick = (info: DateClickArg) => {
   let event = {
     start: `${info.dateStr} 12:00:00`,
     end: `${info.dateStr} 13:00:00`,
-    title: "吃饭",
+    title: "编程",
   };
   events.value.push(event);
 };
 let eventContent = (arg: EventContentArg) => {
   let el = document.createElement("div");
   let timeTextArr = arg.timeText.split(" - ");
-  let start = timeTextArr[0]
-    .replace("上午", "")
-    .replace("下午", "")
-    .replace("时", "");
-  let end = timeTextArr[1]
-    .replace("上午", "")
-    .replace("下午", "")
-    .replace("时", "");
+  console.log(timeTextArr);
+  let start = timeTextArr[0].replace("时", "");
+  let end = timeTextArr[1].replace("时", "");
+  console.log(start, end);
   el.innerHTML = `
         <img src="src/assets/logo.png" style="width:20px;height:20px;">
-         <div>开始时间: ${start}</div>
-         <div>结束时间: ${end}</div>
+         <div>开始时间: ${start}:00</div>
+         <div>结束时间: ${end}:00</div>
          <div>标题: ${arg.event._def.title}</div>
         `;
   return {
